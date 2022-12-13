0be0f26e69ce90dd90b57fbdb0a57e0371dcc0b1
Akshay <akshay.iyer@skyspecs.com>
2022-12-13 06:02:08 +0000

    debug dalle prompt error

--- src/openai_api/prompt_processor.py
+++ src/openai_api/prompt_processor.py
@@ -124,7 +124,7 @@ def create_gpt_metadata(gpt_json):
 
 def create_dalle_prompt(gpt_response):
     #gpt_response = dalle_json['gpt_response']
-    dalle_prompt = gpt_response.split("Visual Description:\n", 2)[1]
+    dalle_prompt = gpt_response.split("Visual Description:", 2)[1]
     print(dalle_prompt)
     return dalle_prompt
 
