6678254b2edce80daaca7dfdf242a009d749c71c
Akshay <akshay.iyer@skyspecs.com>
2022-12-13 06:02:23 +0000

    logging recipe and image for later debugging

--- src/openai_api/run_app.py
+++ src/openai_api/run_app.py
@@ -5,24 +5,31 @@ import json
 import os
 import matplotlib.pyplot as plt
 
+from datetime import datetime
+from pytz import timezone
+
 here = os.path.dirname(os.path.abspath(__file__))
 
 
 def run(input_json):
+    fmt = "%Y%m%d-%H:%M:%S"
+    now_time = datetime.now(timezone('US/Eastern'))
+    timestr = now_time.strftime(fmt)
     gpt_prompt = prompt_processor.create_gpt_prompt(input_json)
     gpt_metadata = prompt_processor.create_gpt_metadata(input_json)
 
     recipe = gpt.get_recipe(
         gpt_prompt, 0.7, 3700, 1)
 
+    with open(f'recipe-{timestr}.txt', 'w') as f:
+        print(recipe, file=f)
+
     dalle_prompt = prompt_processor.create_dalle_prompt(recipe)
 
     images = dalle.generate_output(dalle_prompt)
 
-    # with open('recipe.txt', 'w') as f:
-    #     print(recipe, file=f)
-    # plt.imshow(images[0])
-    # plt.savefig('dish.png')
+    plt.imshow(images[0])
+    plt.savefig(f'dish-{timestr}.png')
 
     return recipe, images
 
