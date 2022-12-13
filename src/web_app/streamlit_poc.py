9e5b956957a2a74554a4c93738a6aadb1e2591c3
shahsagar <sagarshah1@yahoo.com>
2022-12-12 21:53:41 -0800

    Add files via upload

--- src/web_app/streamlit_poc.py
+++ src/web_app/streamlit_poc.py
@@ -7,38 +7,56 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
 from openai_api import run_app
 import json
 
-if "myingredients" not in st.session_state:
-    st.session_state.myingredients = []
+st.set_page_config(layout="wide")
 
-if "rerun" not in st.session_state:
-    st.session_state.rerun = False
-
+if "input" not in st.session_state:
+    st.session_state.input = {'ing_list':[]}
 
 def list_ingredients():
-    st.session_state.tskclk = []
-    for i, ingredient in enumerate(st.session_state.myingredients):
+    for i, ingredient in enumerate(st.session_state.input['ing_list']):
         st.write(ingredient)
 
+cols = st.columns(9)
 
-if st.session_state.rerun:
-    st.session_state.rerun = False
-    st.experimental_rerun()
-else:
-    ingredient = st.text_input(
-        'Enter your ingredients', value="", placeholder='Enter an ingredient')
-    if st.button('Add Task') and ingredient != "" and ingredient not in st.session_state.myingredients:
-        st.session_state.myingredients.append(ingredient)
+with cols[0]:
+    # st.header("ENTER INGREDIENTS [REQUIRED]")
+    ingredient = st.text_input('ENTER INGREDIENTS [REQUIRED]', value="", placeholder = 'Enter an ingredient')
+    if ingredient != "" and ingredient not in st.session_state.input['ing_list']:
+        st.session_state.input['ing_list'].append(ingredient)
+    list_ingredients()
 
-list_ingredients()
+with cols[1]:
+    # st.header("Dietary Restriction")
+    st.session_state.input['food_category'] = st.selectbox('DIETARY RESTRICTIONS', ['any', 'vegan', 'vegetarian', 'non-vegetarian', 'kosher', 'halal'])
 
-if st.button('Generate recipe'):
-    # sagar will fetch input json
-    # XXX: replace this with Sagar's input
-    with open('/home/ubuntu/pytorchbearers/src/openai_api/gpt_json.json') as f:
-        input_json = json.loads(f.read())
+with cols[2]:
+    st.session_state.input['time'] = st.selectbox('TIME', ['any', 'breakfast', 'lunch', 'dinner', 'midnight snack'])
 
-    recipe, images = run_app.run(input_json)
+with cols[3]:
+    st.session_state.input['cuisine_type'] = st.selectbox('CUISINE', ['Any', 'Indian', 'Thai', 'Japanese', 'Chinese', 'Italian', 'French'])
+
+with cols[4]:
+    st.session_state.input['flavor'] = st.selectbox('FLAVOUR PROFILE', ['Any', 'sweet', 'spicy', 'salty', 'bitter', 'sour', 'umami'])
+
+with cols[5]:
+    st.session_state.input['allergies_list'] = st.multiselect('ALLERGIES', ['gluten', 'nuts', 'fish', 'dairy', 'eggs', 'soy'])
+
+with cols[6]:
+    st.session_state.input['meal_type'] = st.selectbox('MEAL TYPE', ['any', 'appetizer', 'entree', 'dessert'])
+
+with cols[7]:
+    st.session_state.input['calorie_count'] = st.slider('NUMBER OF CALORIES', 0, 4000, 500)
+
+with cols[8]:
+    st.session_state.input['prep_time'] = st.slider('Preparation Time (in minutes)', 0, 120, 30)
+
+clicked = st.button('Generate recipe')
+if 'response' in st.session_state or (clicked and st.session_state.input['ing_list']):
+    recipe, images = run_app.run(st.session_state.input)
 
     st.write(recipe)
-    # XXX: display multiple images
-    st.image(images[0])
+    cols = st.columns(len(images))
+    for i in range(len(images)):
+        with cols[i]:
+            st.image(images[i])
+
