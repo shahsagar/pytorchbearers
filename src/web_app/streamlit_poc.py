652d93144ca458573476db04f6def8831acb3a44
Akshay <akshay.iyer@skyspecs.com>
2022-12-15 01:37:10 +0000

    front end: - added loading symbol - changed theme to default dark - changed alignment - added disclaimer - added intro message

--- src/web_app/streamlit_poc.py
+++ src/web_app/streamlit_poc.py
@@ -18,37 +18,50 @@ def list_ingredients():
         st.write(ingredient)
 
 
+st.markdown('<div style="text-align: center;">Hello Foodies, welcome!</div>',
+            unsafe_allow_html=True)
+st.write('')
+st.markdown('<div style="text-align: center;">Whether you are in a soup with only a few items in your refrigator or have a lot of ingredients but are in a pickle with what different you could do - Do not worry, we got you covered!</div>',
+            unsafe_allow_html=True)
+st.write('')
+st.markdown('<div style="text-align: center;">Just list down your ingredients below. You are free to specify other options or leave it as is.</div>',
+            unsafe_allow_html=True)
+st.write('')
+
+# st.write('Hello Foodies, welcome!')
+# st.write('')
+# st.write('Just list down your ingredients below. You are free to specify other options or leave it as is.')
+
 cols = st.columns(9)
 
 with cols[0]:
     # st.header("ENTER INGREDIENTS [REQUIRED]")
+
     ingredient = st.text_input(
-        'ENTER INGREDIENTS [REQUIRED]', value="", placeholder='Enter an ingredient')
+        'ENTER INGREDIENTS*', value="", placeholder='Enter an ingredient')
     if ingredient != "" and ingredient not in st.session_state.input['ing_list']:
         st.session_state.input['ing_list'].append(ingredient)
     list_ingredients()
 
 with cols[1]:
-    # st.header("Dietary Restriction")
-    st.session_state.input['food_category'] = st.selectbox('DIETARY RESTRICTIONS', [
-                                                           'Any', 'Vegan', 'Vegetarian', 'Non vegetarian', 'Kosher', 'Halal'])
-
-# XXX: integrate this in prompt processor or combine here
-with cols[2]:
-    st.session_state.input['time'] = st.selectbox(
-        'TIME', ['Any', 'Breakfast', 'Lunch', 'Dinner', 'Midnight Snack'])
-
-with cols[3]:
     st.session_state.input['cuisine_type'] = st.selectbox(
         'CUISINE', ['Any', 'Indian', 'Thai', 'Japanese', 'Chinese', 'Italian', 'French'])
 
-with cols[4]:
+with cols[2]:
     st.session_state.input['flavor'] = st.selectbox(
         'FLAVOUR PROFILE', ['Any', 'Sweet', 'Spicy', 'Salty', 'Bitter', 'Sour', 'Umami'])
 
+with cols[3]:
+    st.session_state.input['diet_restriction'] = st.selectbox('DIETARY RESTRICTIONS', [
+        'Any', 'Vegan', 'Vegetarian', 'Non vegetarian', 'Kosher', 'Halal'])
+
+with cols[4]:
+    st.session_state.input['time_of_meal'] = st.selectbox(
+        'TIME OF MEAL', ['Any', 'Breakfast', 'Lunch', 'Dinner', 'Midnight Snack'])
+
 with cols[5]:
     st.session_state.input['allergies_list'] = st.multiselect(
-        'ALLERGIES', ['Gluten', 'Nuts', 'Fish', 'Dairy', 'Eggs', 'Soy'])
+        'ALLERGIES', ['None', 'Gluten', 'Nuts', 'Fish', 'Dairy', 'Eggs', 'Soy'], default='None')
 
 with cols[6]:
     st.session_state.input['meal_type'] = st.selectbox(
@@ -60,18 +73,38 @@ with cols[7]:
 
 with cols[8]:
     st.session_state.input['prep_time'] = st.slider(
-        'Preparation Time (in minutes)', 0, 120, 30)
+        'PREP TIME', 0, 120, 30)
 
-clicked = st.button('Generate recipe')
+cols = st.columns(5)
+with cols[0]:
+    pass
+with cols[1]:
+    pass
+with cols[2]:
+    clicked = st.button('Generate recipe')
+with cols[3]:
+    pass
+with cols[4]:
+    pass
+
 if 'response' in st.session_state or (clicked and st.session_state.input['ing_list']):
-    recipe, images = run_app.run(st.session_state.input)
+    with st.spinner(text="Creating something yummy for you..."):
+        try:
+            recipe, dalle_prompt = run_app.run_gpt(st.session_state.input)
 
-    if(recipe):
-        st.write(recipe)
-    if(images):
-        cols = st.columns(len(images))
-        for i in range(len(images)):
-            with cols[i]:
-                st.image(images[i])
+            if (recipe):
+                st.write(recipe)
 
-    #TODO -> Add the case for showing user helpful message in case of recipe blank and images
+            images = run_app.run_dalle(dalle_prompt)
+
+            if (images):
+                cols = st.columns(len(images))
+                for i in range(len(images)):
+                    with cols[i]:
+                        st.image(images[i])
+
+            st.write('P.S.: Remember, this recipe is generated using artificial intelligence. Kindly use your natural intelligence to decide if it is right for you :)')
+        except Exception as e:
+            print('Something went wrong. Please try again')
+
+        # TODO -> Add the case for showing user helpful message in case of recipe blank and images
