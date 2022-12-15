f77f57411ae7f1bb2f76a8449a6b473409298499
Akshay <akshay.iyer@skyspecs.com>
2022-12-15 06:15:18 +0000

    getting run app within streamlit

--- src/web_app/streamlit_poc.py
+++ src/web_app/streamlit_poc.py
@@ -3,8 +3,114 @@ from datetime import datetime as dt
 import streamlit as st
 import os
 import sys
+import matplotlib.pyplot as plt
+import openai
+import requests
+from io import BytesIO
+from PIL import Image
+
+from datetime import datetime
+from pytz import timezone
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
 from openai_api import run_app, prompt_processor
+
+
+def generate_output(prompt, number_of_results=1, size="1024x1024"):
+    try:
+        response = openai.Image.create(
+            prompt=prompt,
+            n=number_of_results,
+            size=size
+        )
+
+        image_urls = response['data']
+        result_images = []
+        for url in image_urls:
+            response = requests.get(url['url'])
+            img = Image.open(BytesIO(response.content))
+            result_images.append(img)
+    except Exception as e:
+        raise Exception("Error occured in calling Dalle image create api", e)
+        return e
+
+    return result_images
+
+
+def extract_prompt(choices):
+    prompts = []
+
+    for choice in choices:
+        if choice and choice.text:
+            prompts.append(choice.text)
+
+    return prompts[0]
+
+
+'''
+Function which takes in prompt and generates a recipe as output
+'''
+
+
+def get_recipe(prompt, temperature=0.7, max_tokens=3700, number_of_results=1):
+    try:
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,  # "i have potatoes, rice, and tofu. Create a 500 calories recipe using these ingredients and show the calorie breakwdown",
+            temperature=temperature,
+            max_tokens=max_tokens,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+        if (response):
+            return extract_prompt(response['choices'])
+
+    except Exception as e:
+        raise Exception("Error occured in calling GPT completion api", e)
+
+    return []
+
+
+def run_gpt(gpt_prompt):
+    fmt = "%Y%m%d-%H:%M:%S"
+    now_time = datetime.now(timezone('US/Eastern'))
+    timestr = now_time.strftime(fmt)
+
+    try:
+        recipe = get_recipe(
+            gpt_prompt, 0.7, 3700, 1)
+        print(recipe)
+        # with open(f'logs/recipe-{timestr}.txt', 'w') as f:
+        #     print(recipe, file=f)
+
+        return recipe
+
+    except Exception as e:
+        print(e)
+        return e
+
+
+def run_dalle(dalle_prompt):
+    fmt = "%Y%m%d-%H:%M:%S"
+    now_time = datetime.now(timezone('US/Eastern'))
+    timestr = now_time.strftime(fmt)
+
+    try:
+        # to check excpetion handling
+        # dalle_prompt = "A plate of spicy Bittergourd Curry with slices of Bittergourd cooked in a creamy sauce of spices and milk. The dish is topped with chopped fresh coriander leaves and served hot."
+
+        # change number of images here
+        images = generate_output(dalle_prompt, 3)
+
+        # plt.imshow(images[0])
+        # plt.savefig(f'logs/dish-{timestr}.png')
+
+        return images
+
+    except Exception as e:
+        print(e)
+        return e
+
 
 st.set_page_config(layout="wide")
 
@@ -92,14 +198,14 @@ if 'response' in st.session_state or (clicked and st.session_state.input['ing_li
             gpt_json = st.session_state.input
             gpt_prompt = prompt_processor.create_gpt_prompt(gpt_json)
 
-            recipe = run_app.run_gpt(gpt_json)
+            recipe = run_gpt(gpt_json)
 
             dalle_prompt = prompt_processor.create_dalle_prompt(recipe)
 
             if (recipe):
                 st.write(recipe)
 
-            images = run_app.run_dalle(dalle_prompt)
+            images = run_dalle(dalle_prompt)
             if (images):
                 cols = st.columns(len(images))
                 for i in range(len(images)):
